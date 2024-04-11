6ac11cc5e9405a88347ce5551b0592c4be516e99
Toshi Aoyama <toaoyama@microsoft.com>
2024-04-11 08:13:50 +0000

    change hello-map.py for exercises 2

--- chapter2/hello-map.py
+++ chapter2/hello-map.py
@@ -5,25 +5,43 @@ from time import sleep
 program = r"""
 BPF_HASH(counter_table);
 
-int hello(void *ctx) {
-   u64 uid;
+int execvefunc(void *ctx) {
+   u64 syscallnum;
    u64 counter = 0;
    u64 *p;
 
-   uid = bpf_get_current_uid_gid() & 0xFFFFFFFF;
-   p = counter_table.lookup(&uid);
+   syscallnum = 59;
+   p = counter_table.lookup(&syscallnum);
    if (p != 0) {
       counter = *p;
    }
    counter++;
-   counter_table.update(&uid, &counter);
+   counter_table.update(&syscallnum, &counter);
    return 0;
 }
+
+int openatfunc(void *ctx) {
+   u64 syscallnum;
+   u64 counter = 0;
+   u64 *p;
+
+   syscallnum = 257;
+   p = counter_table.lookup(&syscallnum);
+   if (p != 0) {
+      counter = *p;
+   }
+   counter++;
+   counter_table.update(&syscallnum, &counter);
+   return 0;
+}
+
 """
 
 b = BPF(text=program)
-syscall = b.get_syscall_fnname("execve")
-b.attach_kprobe(event=syscall, fn_name="hello")
+execve_syscall = b.get_syscall_fnname("execve")
+openat_syscall = b.get_syscall_fnname("openat")
+b.attach_kprobe(event=execve_syscall, fn_name="execvefunc")
+b.attach_kprobe(event=openat_syscall, fn_name="openatfunc")
 
 # Attach to a tracepoint that gets hit for all syscalls 
 # b.attach_raw_tracepoint(tp="sys_enter", fn_name="hello")
