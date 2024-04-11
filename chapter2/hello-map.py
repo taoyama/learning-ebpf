0db207f01584f59a050bb0fd54c52b0425c010f7
Toshi Aoyama <toaoyama@microsoft.com>
2024-04-11 10:41:41 +0000

    change hello-map.py for exercises 3

--- chapter2/hello-map.py
+++ chapter2/hello-map.py
@@ -5,43 +5,25 @@ from time import sleep
 program = r"""
 BPF_HASH(counter_table);
 
-int execvefunc(void *ctx) {
-   u64 syscallnum;
+int sysenterfunc(struct bpf_raw_tracepoint_args *ctx) {
+   u64 uid;
    u64 counter = 0;
    u64 *p;
 
-   syscallnum = 59;
-   p = counter_table.lookup(&syscallnum);
+   uid = bpf_get_current_uid_gid() & 0xFFFFFFFF;
+   p = counter_table.lookup(&uid);
    if (p != 0) {
       counter = *p;
    }
    counter++;
-   counter_table.update(&syscallnum, &counter);
-   return 0;
-}
-
-int openatfunc(void *ctx) {
-   u64 syscallnum;
-   u64 counter = 0;
-   u64 *p;
-
-   syscallnum = 257;
-   p = counter_table.lookup(&syscallnum);
-   if (p != 0) {
-      counter = *p;
-   }
-   counter++;
-   counter_table.update(&syscallnum, &counter);
+   counter_table.update(&uid, &counter);
    return 0;
 }
 
 """
 
 b = BPF(text=program)
-execve_syscall = b.get_syscall_fnname("execve")
-openat_syscall = b.get_syscall_fnname("openat")
-b.attach_kprobe(event=execve_syscall, fn_name="execvefunc")
-b.attach_kprobe(event=openat_syscall, fn_name="openatfunc")
+b.attach_raw_tracepoint(tp="sys_enter", fn_name="sysenterfunc")
 
 # Attach to a tracepoint that gets hit for all syscalls 
 # b.attach_raw_tracepoint(tp="sys_enter", fn_name="hello")
