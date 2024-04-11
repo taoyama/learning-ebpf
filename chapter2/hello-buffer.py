43773bed90db666a27a7cacc776306b42a65d093
Toshi Aoyama <toaoyama@microsoft.com>
2024-04-11 04:31:00 +0000

    change hello-buffer.py for exercises 1

--- chapter2/hello-buffer.py
+++ chapter2/hello-buffer.py
@@ -4,9 +4,10 @@ from bcc import BPF
 program = r"""
 BPF_PERF_OUTPUT(output); 
  
-struct data_t {     
+struct data_t {
    int pid;
    int uid;
+   char evenodd[5];
    char command[16];
    char message[12];
 };
@@ -14,11 +15,17 @@ struct data_t {
 int hello(void *ctx) {
    struct data_t data = {}; 
    char message[12] = "Hello World";
+   char even[5] = "EVEN";
+   char odd[5] = "ODD";
  
    data.pid = bpf_get_current_pid_tgid() >> 32;
    data.uid = bpf_get_current_uid_gid() & 0xFFFFFFFF;
-   
+
    bpf_get_current_comm(&data.command, sizeof(data.command));
+   if (data.pid % 2)
+      bpf_probe_read_kernel(&data.evenodd, sizeof(data.evenodd), even);
+   else
+      bpf_probe_read_kernel(&data.evenodd, sizeof(data.evenodd), odd);
    bpf_probe_read_kernel(&data.message, sizeof(data.message), message); 
  
    output.perf_submit(ctx, &data, sizeof(data)); 
@@ -33,7 +40,7 @@ b.attach_kprobe(event=syscall, fn_name="hello")
  
 def print_event(cpu, data, size):  
    data = b["output"].event(data)
-   print(f"{data.pid} {data.uid} {data.command.decode()} {data.message.decode()}")
+   print(f"{data.pid} {data.uid} {data.evenodd.decode()} {data.command.decode()} {data.message.decode()}")
  
 b["output"].open_perf_buffer(print_event) 
 while True:   
