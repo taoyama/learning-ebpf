e4fbadbb723014486cd468e46805c42ee92573cf
Toshi Aoyama <toaoyama@microsoft.com>
2024-04-11 11:43:59 +0000

    change hello-map.py for excercises 5

--- chapter2/hello-map.py
+++ chapter2/hello-map.py
@@ -3,27 +3,29 @@ from bcc import BPF
 from time import sleep
 
 program = r"""
+#pragma clang diagnostic ignored "-Wint-conversion"
 BPF_HASH(counter_table);
+BPF_PROG_ARRAY(syscall, 300);
 
-int sysenterfunc(struct bpf_raw_tracepoint_args *ctx) {
-   u64 uid;
+RAW_TRACEPOINT_PROBE(sys_enter) {
+   u64 syscallnum = ctx->args[1];
    u64 counter = 0;
    u64 *p;
 
-   uid = bpf_get_current_uid_gid() & 0xFFFFFFFF;
-   p = counter_table.lookup(&uid);
+   syscall.call(ctx, syscallnum);
+   p = counter_table.lookup(&syscallnum);
    if (p != 0) {
       counter = *p;
    }
    counter++;
-   counter_table.update(&uid, &counter);
+   counter_table.update(&syscallnum, &counter);
    return 0;
 }
 
 """
 
 b = BPF(text=program)
-b.attach_raw_tracepoint(tp="sys_enter", fn_name="sysenterfunc")
+#b.attach_raw_tracepoint(tp="sys_enter", fn_name="sysenterfunc")
 
 # Attach to a tracepoint that gets hit for all syscalls 
 # b.attach_raw_tracepoint(tp="sys_enter", fn_name="hello")
